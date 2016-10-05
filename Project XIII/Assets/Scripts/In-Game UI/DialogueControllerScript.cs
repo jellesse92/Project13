85b623102ca1b61f35fa75b31fa86cfeef19af9f
Arielle Chongco <achongco@uci.edu>
2016-10-04 20:16:21 -0700

    Minor prep for dialogue stuff

--- Project XIII/Assets/Scripts/In-Game UI/DialogueControllerScript.cs
+++ Project XIII/Assets/Scripts/In-Game UI/DialogueControllerScript.cs
@@ -1,9 +1,20 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class DialogueControllerScript : MonoBehaviour {
 
+    //UI objects for Dialogue
+    public GameObject dialogueUI;           //UI for dialogue to be activated when dialogue is played
+    public GameObject skipUIPanel;          //For skipping dialogue. Jazz's favorite feature
+    public Image leftPortrait;              //Left speaking portrait
+    public Image rightPortrait;             //Right speaking portrait
+    public GameObject leftNameTag;          //Left character portrait's name tag
+    public GameObject rightNameTag;         //Right character portrait's name tag
+    public Text dialogueText;               //Typed text for dialogue
     
+
+
 
 	// Use this for initialization
 	void Start () {
