054449796e53f869cb6059383e92a0d648c3a06d
Arielle Chongco <achongco@uci.edu>
2016-10-06 16:13:31 -0700

    Made animation template and some filler animation for enemies.
    Move the player closer to the enemies in the enemy test scene to demo

--- Project XIII/Assets/Scripts/Enemy3D/BasicEnemy.cs
+++ Project XIII/Assets/Scripts/Enemy3D/BasicEnemy.cs
@@ -3,19 +3,55 @@ using System.Collections;
 
 public class BasicEnemy : Enemy {
 
+    Animator anim;
+
 	// Use this for initialization
 	void Start () {
         fullHealth = 100;
         health = 100;
+
+
+        anim = GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-        if(isVisible && inPursuit)
+        if (isVisible && inPursuit)
         {
             transform.position = Vector2.MoveTowards(transform.position, target.transform.position, .03f);
+            AnimationCheck();
         }
+        else
+            anim.SetBool("isIdle",true);
     }
 
+    //Checks what animation to play
+    void AnimationCheck()
+    {
+        int x = 0;
+        int y = 0;
+
+        if (target.transform.position.y > transform.position.y)
+            y = 1;
+        else if (target.transform.position.y < transform.position.y)
+            y = -1;
+        else if (target.transform.position.x > transform.position.x)
+            x = 1;
+        else if (target.transform.position.x < transform.position.x)
+            x = -1;
+
+        if (y == 0 && x == 0)
+        {
+            anim.SetBool("isIdle", true);
+        }
+        else
+        {
+            anim.SetBool("isIdle", false);
+        }
+
+        anim.SetInteger("x", x);
+        anim.SetInteger("y", y);
+
+    }
 
 }
