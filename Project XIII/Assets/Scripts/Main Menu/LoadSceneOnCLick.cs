979cc0157b736c5110d65c3f89152f3c05a402f2
clpachec <carlleninpacheco@gmail.com>
2016-10-06 09:08:56 -0700

    Functional Main Menu Draft

---
+++ Project XIII/Assets/Scripts/Main Menu/LoadSceneOnCLick.cs
@@ -0,0 +1,12 @@
+﻿ using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LoadSceneOnCLick : MonoBehaviour {
+
+	// Use this for initialization
+	public void LoadByIndex(int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex);
+    }
+}
---
+++ Project XIII/Assets/Scripts/Main Menu/QuitButton.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class QuitButton : MonoBehaviour {
+
+	// Use this for initialization
+	public void Quit()
+    {
+        UnityEditor.EditorApplication.isPlaying = false;
+        Application.Quit();
+    }
+}
