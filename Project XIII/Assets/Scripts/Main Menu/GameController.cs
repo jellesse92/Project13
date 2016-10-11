28e6162dacceba0209c1212c4f9ff80b6bb2feba
Nick Smith <smithne@uci.edu>
2016-10-11 03:35:11 -0700

    Single Player Character Select
    a blackout and 40 merge conflicts later, it now works for single player,
    links to the correct scenes, and ~sort of~ uses carl's style.

---
+++ Project XIII/Assets/Scripts/Main Menu/GameController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class GameController : MonoBehaviour
+{
+    public static GameController Instance
+    {
+        get;
+        set;
+    }
+
+    void Awake()
+    {
+        DontDestroyOnLoad(transform.gameObject);
+        Instance = this;
+    }
+
+    public int character = 0;
+
+    public void SetChar(int CharType)
+    {
+        character = CharType;
+    }
+}
