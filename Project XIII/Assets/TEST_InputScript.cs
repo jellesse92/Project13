e646d47b8a0b3f1d18de33067ae3390c232403a6
Arielle Chongco <achongco@uci.edu>
2016-10-11 00:01:05 -0700

    Set up project to be able to take PS4
    controller controls if it's Player 1

---
+++ Project XIII/Assets/TEST_InputScript.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TEST_InputScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        /*
+        if(Input.GetAxis("1_LeftJoyStickX") != 0)
+            Debug.Log("Getting P1 - LeftX: " + Input.GetAxis("LeftJoyStickX1"));
+        if(Input.GetAxis("1_LeftJoyStickY") != 0)
+            Debug.Log("Getting P1 - LeftY: " + Input.GetAxis("LeftJoyStickY1"));
+        if (Input.GetAxis("1_RightJoyStickX") != 0)
+            Debug.Log("Getting P1 - RightX: " + Input.GetAxis("RightJoyStickX1"));
+        if (Input.GetAxis("1_RightJoyStickY") != 0)
+            Debug.Log("Getting P1 - RightY: " + Input.GetAxis("RightJoyStickY1"));
+            */
+
+        if (Input.GetButtonDown("1_X"))
+            Debug.Log("Player 1: X Pressed!");
+        if (Input.GetButtonDown("1_Triangle"))
+            Debug.Log("Player 1: Triangle Pressed!");
+        if (Input.GetButtonDown("1_Square"))
+            Debug.Log("Player 1: Square Pressed!");
+        if (Input.GetButtonDown("1_Circle"))
+            Debug.Log("Player 1: Circle Pressed!");
+    }
+}
