7a627bf51aab0606ee57dd2b52572bff1797081e
Arielle Chongco <achongco@uci.edu>
2016-10-14 11:34:00 -0700

    Beginning new environmental elemental stuff
    like a squishy block that makes camera shake!

---
+++ Project XIII/Assets/Scripts/CamShakeScript.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CamShakeScript : MonoBehaviour {
+    //Credit: http://newbquest.com/2014/06/the-art-of-screenshake-with-unity-2d-script/
+
+
+    Vector3 origin;
+
+    float shakeAmount;
+
+
+    void Start()
+    {
+        origin = transform.position;
+        //StartShake(.1f);
+    }
+
+    public void StartShake(float magnitude)
+    {
+        shakeAmount = magnitude;
+        InvokeRepeating("CameraShake", 0, .01f);
+        Invoke("StopShaking", 0.3f);
+    }
+
+
+
+    void CameraShake()
+    {
+        float quakeAmt = Random.value * shakeAmount * 2 - shakeAmount;
+        Vector3 pp = transform.position;
+        pp.y += quakeAmt; // can also add to x and/or z
+        transform.position = pp;
+    }
+
+    void StopShaking()
+    {
+        CancelInvoke("CameraShake");
+        transform.position = origin;
+    }
+}
---
+++ Project XIII/Assets/Scripts/Environmental/SquishBlockScript.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SquishBlockScript : MonoBehaviour {
+
+    Vector3 origin;
+    Vector3 destination;
+
+    public float moveSpeed;
+    public float returnSpeed;
+    public float returnDelay;
+
+    //Determine if block is moving towards destination or returning to origin
+    bool moving = false;
+    bool returning = false;
+
+	// Use this for initialization
+	void Start () {
+        origin = transform.position;
+
+        //Testing. Remove so parent script may control
+        TriggerMove();
+	}
+
+    void OnBecameVisible()
+    {
+        transform.position = origin;
+    }
+
+    void OnBecameInvisible()
+    {
+        transform.position = origin;
+        moving = false;
+        returning = false;
+    }
+
+    void FixedUpdate()
+    {
+        if (moving)
+            MoveTowardsDestination();
+        else if (returning)
+            ReturnToOrigin();
+    }
+
+    public void TriggerMove()
+    {
+        if(!returning)
+            moving = true;
+    }
+
+    void MoveTowardsDestination()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, destination, moveSpeed);
+        if(transform.position == destination)
+        {
+            moving = false;
+            StartCoroutine("DelayReturn");
+        }
+    }
+
+    void ReturnToOrigin()
+    {
+        returning = true;
+        transform.position = Vector2.MoveTowards(transform.position, origin, returnSpeed);
+        if (transform.position == origin)
+        {
+            returning = false;
+        }
+
+    }
+
+    IEnumerator DelayReturn()
+    {
+        yield return new WaitForSeconds(returnDelay);
+        returning = true;
+    }
+	
+
+
+}
