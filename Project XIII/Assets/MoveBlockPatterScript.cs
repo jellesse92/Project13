8f176a44e529b53c28d70e8c0bb35fb9e30c6060
Arielle Chongco <achongco@uci.edu>
2016-10-16 11:41:49 -0700

    finished block pattern thing

---
+++ Project XIII/Assets/MoveBlockPatterScript.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveBlockPatterScript : MonoBehaviour {
+
+    [System.Serializable]
+    public class MoveBlock                              //Contains info on which block ton control and when to trigger movement
+    {
+        public GameObject block;                        //Block to move
+        public float delayStartMove;                    //Time to start after end of previous block sequence
+    }
+
+    [System.Serializable]
+    public class BlockPattern                           //Pattern for current sequence of block movement
+    {
+        public MoveBlock[] moveBlock;
+        public float delayNextPattern;                  //Amount of time to delay starting next sequence
+    }
+
+    public BlockPattern[] blockPattern;                 //Pattern of block movement
+
+    //Private variables controlling flow of block patterns
+    private int currentSequence = 0;                    //Current sequence being examined
+
+    // Use this for initialization
+    void Start () {
+        StartPattern();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+	
+	}
+
+    void StartPattern()
+    {
+        if (currentSequence >= blockPattern.Length)
+            currentSequence = 0;
+        foreach(MoveBlock movBlock in blockPattern[currentSequence].moveBlock)
+        {
+            StartCoroutine(waitToTrigger(movBlock.block, movBlock.delayStartMove));
+        }
+        currentSequence++;
+        Invoke("StartPattern", blockPattern[currentSequence - 1].delayNextPattern);
+    }
+
+    IEnumerator waitToTrigger(GameObject block, float waitTime)
+    {      
+        yield return new WaitForSeconds(waitTime);
+        block.GetComponent<SquishBlockScript>().TriggerMove();
+    }
+}
