bca26e8df659fe7658e5016fb2a2d5173fffdfa5
Arielle Chongco <achongco@uci.edu>
2016-10-18 16:15:42 -0700

    Player select basic layout mostly done
    requires testing for 2+ controllers though. Filler animations for player
    select in too

--- Project XIII/Assets/Scripts/Player Select/ReticleScript.cs
+++ Project XIII/Assets/Scripts/Player Select/ReticleScript.cs
@@ -6,6 +6,7 @@ public class ReticleScript : MonoBehaviour {
     const float RETICLE_SPEED = 10f;        //Speed reticle set to move at
 
     public GameObject selectedCharPanel;    //Panel for selected characters
+    Animator selectedCharAnim;              //Selected Character Animation
 
     public int player;                      //Player to control reticle
     Vector2 moveDir;                        //Direction to move
@@ -18,6 +19,7 @@ public class ReticleScript : MonoBehaviour {
         xInputAxis = player.ToString() + "_LeftJoyStickX";
         yInputAxis = player.ToString() + "_LeftJoyStickY";
         moveDir = new Vector2();
+        selectedCharAnim = selectedCharPanel.GetComponent<Animator>();
     }
 
     void Update()
@@ -41,12 +43,40 @@ public class ReticleScript : MonoBehaviour {
 
     void OnTriggerEnter2D(Collider2D col)
     {
-        Debug.Log(col.name);
+        if (col.name == "Gunner Portrait")
+            ExamineChar("gunnerSelected", 0);
+        else if (col.name == "Swordsman Portrait")
+            ExamineChar("swordsmanSelected", 1);
+        else if (col.name == "Mage Portrait")
+            ExamineChar("mageSelected", 2);
+        else if (col.name == "Mech Portrait")
+            ExamineChar("mechSelected", 3);
     }
 
+    //Character last examined or passed over
+    void ExamineChar(string animName,int charNum)
+    {
+        selectedCharAnim.SetTrigger(animName);
+        //Do something with second parameter to determine character selected
+    }
+
+    //Watch for mouse movement and input if player 1
     void WatchForMouseInput()
     {
         if (Input.GetAxis("Mouse X") != 0 || Input.GetAxis("Mouse Y") != 0)
             transform.position = Input.mousePosition;
+    }
+
+
+    public void ActivateSelectedPanel()
+    {
+        //If you want to add an animation to the selected character panel upon joining a game
+    }
+
+    //Function to call when player decided to leave game
+    public void LeaveSelect()
+    {
+        selectedCharAnim.SetTrigger("exit");
+        //gameObject.SetActive(false);
     }
 }
