239edbae5194085ac3d6b851bc63bb05b2771273
clpachec <carlleninpacheco@gmail.com>
2016-10-27 02:19:01 -0700

    Adjusted Sound Activation

---
+++ Project XIII/Assets/Scripts/AnimationEventSound.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimationEventSound : MonoBehaviour {
+    private AudioSource myAudio;
+    
+    void Start () {
+        myAudio = GetComponent<AudioSource>();
+	}
+	
+    public void playSound(AudioClip clip)
+    {
+        myAudio.clip = clip;
+        myAudio.Play();
+    }
+}
