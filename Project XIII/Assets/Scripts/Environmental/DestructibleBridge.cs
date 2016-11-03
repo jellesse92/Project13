697a8d050455d43b7c017e8f2fec151698c50d4b
jellesse92 <jehunter@uci.edu>
2016-11-03 02:20:31 -0700

    Animations, script, and that thing for that thing. Uhm. Huh.

---
+++ Project XIII/Assets/Scripts/Environmental/DestructibleBridge.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestructibleBridge : MonoBehaviour {
+
+    private bool isUpright;
+    private Destructible d;
+	// Use this for initialization
+	void Start () {
+        isUpright = true;
+        d = gameObject.GetComponent<Destructible>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (d.isDestroyed && isUpright)
+        {
+            bridgeDestroyed();
+        }
+	}
+
+    private void bridgeDestroyed()
+    {
+        isUpright = false;
+        gameObject.tag = "Ground";
+        //Temporary until bridge becomes useable.
+        gameObject.layer = 14;
+    }
+}
