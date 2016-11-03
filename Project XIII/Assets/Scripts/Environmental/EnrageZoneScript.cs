ee3ada18e392d1adb881efe7d30428e3d8a06214
Arielle Chongco <achongco@uci.edu>
2016-11-03 00:32:32 -0700

    prep work stuff for demo

--- Project XIII/Assets/Scripts/Environmental/EnrageZoneScript.cs
+++ Project XIII/Assets/Scripts/Environmental/EnrageZoneScript.cs
@@ -1,18 +1,32 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class EnrageZoneScript : MonoBehaviour {
+
+    HashSet<GameObject> playersinRange = new HashSet<GameObject>();
 
     void OnTriggerEnter2D(Collider2D col)
     {
         Debug.Log("test ");
         if(col.tag == "Player")
         {
-            foreach (Transform child in transform)
-            {
-                child.gameObject.GetComponent<Enemy>().SetTarget(col.gameObject);
-                child.gameObject.GetComponent<Enemy>().SetPursuitState(true);
-            }
+            playersinRange.Add(col.gameObject);
+
+        }
+    }
+
+    public void EnrageEnemies()
+    {
+        Random random = new Random();
+        foreach (Transform child in transform)
+        {
+
+            
+
+
+            //child.gameObject.GetComponent<Enemy>().SetTarget(col.gameObject);
+            child.gameObject.GetComponent<Enemy>().SetPursuitState(true);
         }
     }
 }
