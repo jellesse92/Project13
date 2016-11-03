dbdf64c2a4c46b6e713c934003d8f8cff9a41cfb
Arielle Chongco <achongco@uci.edu>
2016-11-02 22:15:48 -0700

    Player effects stuff

---
+++ Project XIII/Assets/Scripts/PlayerEffectsManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerEffectsManager : MonoBehaviour {
+
+    GameObject screenFlash;
+    GameObject screenShake;
+
+	// Use this for initialization
+	void Start () {
+        screenFlash = GameObject.FindGameObjectWithTag("Hit Flash Image");
+        screenShake = GameObject.FindGameObjectWithTag("MainCamera");
+    }
+
+    //Gets the screen to flash
+    public void FlashScreen()
+    {
+        screenFlash.GetComponent<Animator>().SetTrigger("hitFlash");
+    }
+
+    //Gets the screen to shake
+    public void ScreenShake(float magnitude)
+    {
+        screenShake.GetComponent<CamShakeScript>().StartShake(magnitude);
+    }
+}
