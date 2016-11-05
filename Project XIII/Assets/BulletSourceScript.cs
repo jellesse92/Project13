1c6ebc53c3fda7a6a7996bf56350f1595e610d27
Arielle Chongco <achongco@uci.edu>
2016-11-04 17:04:21 -0700

    Example of knockback commented out
    under quick attack

--- Project XIII/Assets/BulletSourceScript.cs
+++ Project XIII/Assets/BulletSourceScript.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class BulletSourceScript : MonoBehaviour {
 
-    const float stunMultiplier = 1f;                //Multiplier for how long enemies should be stunned after hit
+    const float STUN_MULTI = 1f;                    //Multiplier for how long enemies should be stunned after hit
 
     LayerMask layermask;                            //Prevent raycast from hitting unimportant layers
     RaycastHit2D hit;                               //What was hit by raycast
@@ -19,14 +19,27 @@ public class BulletSourceScript : MonoBehaviour {
     {
 
         hit = Physics2D.Raycast(transform.position, transform.right * transform.parent.localScale.x, 50, layermask);
+
         if (hit.collider != null)
         {
+            Debug.Log(hit.collider.name);
             //make a spark at the hit.point
             if (hit.collider.tag == "Enemy")
-                hit.collider.GetComponent<Enemy>().Damage(damage, stunMultiplier);
+            {
+                ApplyQuickDamage(hit.collider.gameObject, damage);
+            }
 
             //Color color = hit ? Color.green : Color.red;
             //Debug.DrawRay(transform.position, transform.right * (50f * transform.parent.localScale.x), color);
+        }
+    }
+
+    void ApplyQuickDamage(GameObject target, int damage)
+    {
+        if(target.tag == "Enemy")
+        {
+            target.GetComponent<Enemy>().Damage(damage, STUN_MULTI);
+            //target.GetComponent<Rigidbody2D>().AddForce(new Vector2(5000f * transform.parent.localScale.x, 20000f));
         }
     }
 }
