0674226a8fff2ec6a650daf65ef0198fb3316714
clpachec <carlleninpacheco@gmail.com>
2016-11-07 22:39:27 -0800

    Gun Particles

---
+++ Project XIII/Assets/GunnerParticles.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GunnerParticles : MonoBehaviour {
+    public GameObject quickAttack;
+    public GameObject heavyAttack;
+
+    void Start () {
+        quickAttack = Instantiate(quickAttack);
+        quickAttack.transform.parent = transform;
+        quickAttack.transform.position = new Vector3(1.5f, 1, quickAttack.transform.position.z);
+
+        heavyAttack = Instantiate(heavyAttack);
+        heavyAttack.transform.parent = transform;
+    }
+
+    public void PlayParticleQuickAttack()
+    {
+        quickAttack.GetComponent<ParticleSystem>().Play();
+    }
+
+    public void PlayParticleHeavyAttack()
+    {
+        heavyAttack.GetComponent<ParticleSystem>().Play();
+    }
+}
