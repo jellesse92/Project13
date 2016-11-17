fafdd0d3536d79eb69b8450997ea2be288c637a0
Arielle Chongco <achongco@uci.edu>
2016-11-17 10:02:09 -0800

    Enemy Speed variation

---
+++ Project XIII/Assets/EnemyGroupController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyGroupController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	    foreach(Transform child in transform)
+        {
+            if (child.tag == "Enemy")
+                child.GetComponent<Enemy>().speed += Random.Range(-.06f, .06f);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
