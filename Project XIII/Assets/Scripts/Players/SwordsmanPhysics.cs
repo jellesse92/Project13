4eec361be284297089c5c81c05056f1d2f0b8074
Arielle Chongco <achongco@uci.edu>
2016-11-16 21:43:01 -0800

    Beginning combo stuff swordsman

---
+++ Project XIII/Assets/Scripts/Players/SwordsmanPhysics.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwordsmanPhysics : PlayerPhysics{
+
+    bool inCombo = false;                       //Checks if swordsman able to combo
+
+    public override void ClassSpecificStart()
+    {
+
+    }
+
+    public override void ClassSpecificUpdate()
+    {
+        if (inCombo)
+            WatchForCombo();
+    }
+
+    public void WatchForCombo()
+    {
+        if (GetComponent<PlayerInput>().getKeyPress().quickAttackPress)
+        {
+            inCombo = false;
+            GetComponent<Animator>().SetTrigger("combo");
+        }
+    }
+
+    public void StartCombo()
+    {
+        inCombo = true;
+    }
+
+    public void FinishCombo()
+    {
+        inCombo = false;
+    }
+}
