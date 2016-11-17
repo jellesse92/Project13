e410ff466f94ed6a05959ec64833a539cb2518ff
Arielle Chongco <achongco@uci.edu>
2016-11-17 00:48:23 -0800

    Drag up attack added

--- Project XIII/Assets/Scripts/Players/SwordsmanPhysics.cs
+++ Project XIII/Assets/Scripts/Players/SwordsmanPhysics.cs
@@ -4,12 +4,14 @@ using System.Collections;
 public class SwordsmanPhysics : PlayerPhysics{
 
     public GameObject comboAttackBox;           //Collider for dealing combo attack damage
+    public GameObject dragAttackBox;            //Collider for dragging enemies with sword swing up or down
 
     bool inCombo = false;                       //Checks if swordsman able to combo
 
     public override void ClassSpecificStart()
     {
         comboAttackBox.GetComponent<SwordsmanMelee>().SetDamage(GetComponent<PlayerProperties>().GetPhysicStats().quickAttackStrength);
+        dragAttackBox.GetComponent<SwordsmanDragAttackScript>().enabled = false;
     }
 
     public override void ClassSpecificUpdate()
@@ -36,8 +38,22 @@ public class SwordsmanPhysics : PlayerPhysics{
     {
         inCombo = false;
     }
+
     public void HeavyTransistionToAir()
     {
         GetComponent<Animator>().SetTrigger("heavyToAerial");
+    }
+
+    public void ExecuteDragAttack()
+    {
+        dragAttackBox.GetComponent<SwordsmanDragAttackScript>().Reset();
+        dragAttackBox.GetComponent<SwordsmanDragAttackScript>().enabled = true;
+    }
+
+    public void EndDragAttack()
+    {
+        dragAttackBox.GetComponent<Collider2D>().enabled = false;
+        dragAttackBox.GetComponent<SwordsmanDragAttackScript>().Reset();
+        dragAttackBox.GetComponent<SwordsmanDragAttackScript>().enabled = false;
     }
 }
---
+++ Project XIII/Assets/SwordsmanDragAttackScript.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class SwordsmanDragAttackScript : MonoBehaviour {
+
+    const float Y_OFFSET = 1f;
+
+    HashSet<GameObject> enemy = new HashSet<GameObject>();
+    int damage = 1;
+    
+
+	// Use this for initialization
+	void Start () {
+        damage = transform.parent.GetComponent<PlayerProperties>().GetPhysicStats().heavyAttackStrength;
+    }
+
+    void FixedUpdate()
+    {
+        foreach (GameObject target in enemy)
+        {
+            target.transform.position = new Vector3(target.transform.position.x, transform.position.y, target.transform.position.z);
+        }
+    }
+
+    public void ApplyDamageEffect()
+    {
+        if (enemy.Count > 0)
+        {
+            if (transform.parent.parent != null)
+                transform.parent.parent.GetComponent<PlayerEffectsManager>().ScreenShake(.01f);
+            //kickSparkEffect.GetComponent<ParticleSystem>().Play();
+            foreach (GameObject target in enemy)
+                target.GetComponent<Enemy>().Damage(transform.parent.GetComponent<PlayerProperties>().GetPhysicStats().heavyAirAttackStrengh, .2f);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.tag == "Enemy")
+            if (!enemy.Contains(col.gameObject))
+            {
+                enemy.Add(col.gameObject);
+
+                if (transform.parent.parent != null)
+                    transform.parent.parent.GetComponent<PlayerEffectsManager>().ScreenShake(.01f);
+
+                col.GetComponent<Enemy>().Damage(transform.parent.GetComponent<PlayerProperties>().GetPhysicStats().heavyAirAttackStrengh, .2f);
+            }
+
+    }
+
+    public void Reset()
+    {
+        enemy = new HashSet<GameObject>();
+    }
+}
