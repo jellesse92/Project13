a6b760d9d11773cdf22d627b01592328105513a0
Arielle Chongco <achongco@uci.edu>
2016-12-05 16:49:08 -0800

    Gunner animation fix

---
+++ Project XIII/Assets/DetonatingSkullPhysics.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetonatingSkullPhysics : EnemyPhysics {
+
+    const float EXPLOSION_DELAY = 1f;               //Amount of time enemy delays explosion after contact with player
+
+
+}
