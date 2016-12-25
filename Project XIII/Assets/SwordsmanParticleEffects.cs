1ef968560d80964eea617bb60dfd74438d50cb12
clpachec <carlleninpacheco@gmail.com>
2016-12-25 05:50:59 -0800

    Added Up attack dust

--- Project XIII/Assets/SwordsmanParticleEffects.cs
+++ Project XIII/Assets/SwordsmanParticleEffects.cs
@@ -4,23 +4,31 @@ using UnityEngine;
 
 public class SwordsmanParticleEffects : PlayerParticleEffects {
     public GameObject chargingDust;
+    public GameObject upAttackDust;
 
     Vector3 positionChargingDust;
     Vector3 positionJumpDust;
     Vector3 positionRunningDust;
+    Vector3 positionUpAttackDust;
 
     protected override void ClassSpecificAwake()
     {
         positionChargingDust = new Vector3(transform.position.x + 0.7f, transform.position.y - 2.5f, transform.position.z);
         positionJumpDust = new Vector3(transform.position.x, transform.position.y - 2.5f, transform.position.z);
         positionRunningDust = new Vector3(transform.position.x - 0.5f, transform.position.y - 2.5f, transform.position.z);
+        positionUpAttackDust = new Vector3(transform.position.x, transform.position.y - 2.5f, transform.position.z);
+
         InstantiateParticle(ref chargingDust);
+        InstantiateParticle(ref upAttackDust);
+
         if (chargingDust)
             chargingDust.transform.position = positionChargingDust;
         if (jumpDust)
             jumpDust.transform.position = positionJumpDust;
         if (runningDust)
             runningDust.transform.position = positionRunningDust;
+        if (upAttackDust)
+            upAttackDust.transform.position = positionUpAttackDust;
     }
 
     public void PlayChargingDust(bool play)
@@ -29,5 +37,10 @@ public class SwordsmanParticleEffects : PlayerParticleEffects {
             chargingDust.GetComponent<ParticleSystem>().Play();
         else
             chargingDust.GetComponent<ParticleSystem>().Stop();
+    }
+
+    public void PlayUpAttackDust()
+    {
+        PlayParticle(upAttackDust);
     }
 }
