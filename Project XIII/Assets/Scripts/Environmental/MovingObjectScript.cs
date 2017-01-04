110a2b228d1f804660b5724b06a606799ae8d6a1
Arielle Chongco <achongco@uci.edu>
2017-01-03 21:57:51 -0800

    More prep stuff

---
+++ Project XIII/Assets/Scripts/Environmental/MovingObjectScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingObjectScript : MonoBehaviour {
+
+    public GameObject movingObject;                         //Object that is to be moved
+    public Transform travelPoints;                          //Points which the object must travel between
+
+    public float moveSpeed = 1f;                            //Speed which object moves between travel points
+    public float moveStartDelay = 5f;                       //Delay before object starts moving again     
+
+    int currentDestination = 1;                             //Destination point currently being traveled too
+    bool moving = true;                 
+
+	// Use this for initialization
+	void Start () {
+        if (travelPoints.childCount > 0)
+            movingObject.transform.position = new Vector3(travelPoints.GetChild(0).position.x, travelPoints.GetChild(0).position.y, movingObject.transform.position.z);
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		
+	}
+
+    void MoveTowardsDestination()
+    {
+
+    }
+
+
+}
