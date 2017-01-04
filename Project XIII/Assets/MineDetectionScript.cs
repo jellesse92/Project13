9f31377fed195b4d35204f631ea9df09d03a6d58
Arielle Chongco <achongco@uci.edu>
2017-01-03 19:16:05 -0800

    Made flashing effect for mine

---
+++ Project XIII/Assets/MineDetectionScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MineDetectionScript : MonoBehaviour {
+
+    public int radiusLevel = 0;
+
+    HashSet<GameObject> playersInRange;
+
+    private void Start()
+    {
+        playersInRange = new HashSet<GameObject>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+            if (!playersInRange.Contains(collision.gameObject))
+            {
+                playersInRange.Add(collision.gameObject);
+                transform.parent.parent.GetComponent<FloatingMineScript>().RadiusReport(radiusLevel, true);
+            }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+            if (playersInRange.Contains(collision.gameObject))
+            {
+                playersInRange.Remove(collision.gameObject);
+                transform.parent.parent.GetComponent<FloatingMineScript>().RadiusReport(radiusLevel, false);
+            }
+    }
+
+    public bool detectsPlayer()
+    {
+        Debug.Log("players?:" + playersInRange.Count);
+        return playersInRange.Count != 0;
+    }
+}
--- Project XIII/Assets/Scripts/Environmental/FloatingMineScript.cs
+++ Project XIII/Assets/Scripts/Environmental/FloatingMineScript.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class FloatingMineScript : MonoBehaviour {
 
+    const float OUTER_DETECTION_FLASH_RATE = .8f;
+    const float MIDDLE_DETECTION_FLASH_RATE = .3f;
+    const float INNER_DETECTION_FLASH_RATE = .1f;
+
     const int DAMAGE = 20;
 
     public ParticleSystem explosion;
+    public Transform detectionRadii;
 
+    private int currentRadiusLevel = 3;             //How close player is to mine. 3 is the farthest level
+    private float[] flashRates = new float[]{ INNER_DETECTION_FLASH_RATE, MIDDLE_DETECTION_FLASH_RATE, OUTER_DETECTION_FLASH_RATE };
 
     private void OnTriggerEnter2D(Collider2D col)
     {
         if(col.tag == "Player")
         {
+            StopAllCoroutines();
             explosion.Play();
             col.gameObject.GetComponent<PlayerProperties>().TakeDamage(DAMAGE);
             GetComponent<SpriteRenderer>().enabled = false;
@@ -25,4 +33,54 @@ public class FloatingMineScript : MonoBehaviour {
         GetComponent<SpriteRenderer>().enabled = true;
         GetComponent<Collider2D>().enabled = true;
     } 
+
+    public void StopFlashing()
+    {
+        StopAllCoroutines();
+        GetComponent<SpriteRenderer>().color = Color.white;
+    }
+
+    public void RadiusReport(int radiusLevel, bool entering)
+    {
+        if (entering)
+        {
+            //Prioritze flashing based on the closest player
+            if(currentRadiusLevel > radiusLevel)
+            {
+                StopFlashing();
+                StartCoroutine(DetectionFlashEffect(flashRates[radiusLevel]));
+                currentRadiusLevel = radiusLevel;
+            }
+        }
+        else
+        {
+            //If the radius which is being exited is the closest detected zone and is being exited
+            //Check if there are still players in the zone excluding the player exiting
+            if(currentRadiusLevel == radiusLevel)
+            {
+                if (!detectionRadii.GetChild(radiusLevel).GetComponent<MineDetectionScript>().detectsPlayer())
+                {
+                    StopFlashing();
+                    currentRadiusLevel++;
+                    if (currentRadiusLevel < 3)
+                    {
+                        StartCoroutine(DetectionFlashEffect(flashRates[radiusLevel]));
+                        Debug.Log("Exit: " + radiusLevel);
+                    }
+                }
+            }
+        }
+    }
+
+    IEnumerator DetectionFlashEffect(float rate)
+    {
+        while (true)
+        {
+            GetComponent<SpriteRenderer>().color = Color.red;
+            yield return new WaitForSeconds(rate);
+            GetComponent<SpriteRenderer>().color = Color.white;
+            yield return new WaitForSeconds(rate);
+        }
+
+    }
 }
