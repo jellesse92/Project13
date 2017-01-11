47ca15fb85750a03f7fc4d65fe93625c62b5f5ca
Arielle Chongco <achongco@uci.edu>
2017-01-10 22:21:51 -0800

    Beginning turret enemy

--- Project XIII/Assets/Scripts/General Enemy/EnemyBulletScript.cs
+++ Project XIII/Assets/Scripts/General Enemy/EnemyBulletScript.cs
@@ -2,20 +2,24 @@
 using System.Collections;
 
 public class EnemyBulletScript : MonoBehaviour {
-    /*
+
+    int damage = 10;
+    
 	void OnTriggerEnter2D(Collider2D col)
     {
         if(col.tag != "Enemy")
         {
-            transform.parent.parent.GetComponent<BasicRangeEnemy>().ReloadAmmo();
+            //transform.parent.parent.GetComponent<BasicRangeEnemy>().ReloadAmmo();
             if (col.tag == "Player")
             {
-                col.gameObject.GetComponent<Player>().TakeDamage(10);
+                col.GetComponent<PlayerProperties>().TakeDamage(damage);
             }
             gameObject.SetActive(false);
         }
-
-
     }
-    */
+    
+    public void SetDamage(int dmg)
+    {
+        damage = dmg;
+    }
 }
---
+++ Project XIII/Assets/Scripts/General Enemy/Turret Enemy/BasicTurretEnemy.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicTurretEnemy : EnemyPhysics
+{
+
+    const int AMMO_AMOUNT = 20;                             //Amount of ammo to be created
+
+    public float rangedAttackCooldown = .1f;                //Cooldown between ranged attack
+
+    public GameObject rangedProjectile;                    //Projectiles to be shot
+    public Transform projectileList;                       //Transform containing projectiles
+
+    // Use this for initialization
+    void Start()
+    {
+        for (int i = 0; i < 20; i++)
+        {
+            GameObject bullet = (GameObject)Instantiate(rangedProjectile);
+            bullet.GetComponent<EnemyBulletScript>().SetDamage(attackPower);
+            bullet.transform.SetParent(projectileList);
+            bullet.transform.position = projectileList.position;
+            bullet.SetActive(false);
+        }
+    }
+
+    public override void ApproachTarget()
+    {
+
+        if (target.transform.position.x > transform.position.x)
+        {
+            if (!facingRight)
+                Turn();
+
+        }
+        else if (target.transform.position.x < transform.position.x)
+        {
+            if (facingRight)
+                Turn();
+
+        }
+    }
+
+    public void Turn()
+    {
+        anim.SetTrigger("Turn");
+        turning = true;
+    }
+
+    public void EndTurn()
+    {
+        Flip();
+        turning = false;
+    }
+
+    public void AttackCDRecovery()
+    {
+        Invoke("SetCanAttackTrue", rangedAttackCooldown);
+    }
+
+    void SetCanAttackTrue()
+    {
+        canAttack = true;
+    }
+}
