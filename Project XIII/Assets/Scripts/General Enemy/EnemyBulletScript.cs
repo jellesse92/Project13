d4e759727e675e8edd2b970c8b657529b66d8d73
clpachec <carlleninpacheco@gmail.com>
2017-01-12 21:22:53 -0800

    Adding impact need to change the bullet script more

--- Project XIII/Assets/Scripts/General Enemy/EnemyBulletScript.cs
+++ Project XIII/Assets/Scripts/General Enemy/EnemyBulletScript.cs
@@ -2,10 +2,35 @@
 using System.Collections;
 
 public class EnemyBulletScript : MonoBehaviour {
+    string FIREBALL_NAME = "Fireball";
+    string IMPACT_NAME = "Impact";
 
     int damage = 10;
-    
-	void OnTriggerEnter2D(Collider2D col)
+    ParticleSystem fireballParticle;
+    ParticleSystem impactParticle;
+
+    void Start()
+    {
+        if (transform.Find(FIREBALL_NAME))
+            fireballParticle = transform.Find(FIREBALL_NAME).GetComponent<ParticleSystem>();
+        if (transform.Find(IMPACT_NAME))
+            impactParticle = transform.Find(IMPACT_NAME).GetComponent<ParticleSystem>();
+    }
+
+    public void PlayParticle(bool play)
+    {
+        Debug.Log(fireballParticle);
+
+        if (play)
+            fireballParticle.Play();
+        else
+        {
+            fireballParticle.Stop();
+            impactParticle.Play();
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
     {
         if(col.tag != "Enemy")
         {
@@ -14,7 +39,8 @@ public class EnemyBulletScript : MonoBehaviour {
             
             if (!col.GetComponent<Collider2D>().isTrigger)
             {
-                gameObject.SetActive(false);
+                //gameObject.SetActive(false);
+                PlayParticle(false);
                 transform.parent.parent.GetComponent<Enemy>().ReloadAmmo();
             }
 
