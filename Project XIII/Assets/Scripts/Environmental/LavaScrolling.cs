7db1a1a24af05334bc7375513c3c702dd65b30b9
clpachec <carlleninpacheco@gmail.com>
2017-01-23 13:01:30 -0800

    Testing Lava Distortion Shader

---
+++ Project XIII/Assets/Scripts/Environmental/LavaScrolling.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LavaScrolling : MonoBehaviour 
+{
+	public Vector2 animationRate = new Vector2( 1.0f, 0.0f );
+	public string textureName = "_MainTex";
+	
+	Vector2 offset = Vector2.zero;
+	
+	void LateUpdate() 
+	{
+        offset += (animationRate * Time.deltaTime );
+		if( GetComponent<Renderer>().enabled )
+		{
+			GetComponent<Renderer>().sharedMaterial.SetTextureOffset( textureName, offset);
+		}
+	}
+}
