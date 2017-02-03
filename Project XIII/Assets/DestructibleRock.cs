b1c12f50ab770a69688ed0e15681d4075b0e06c5
clpachec <carlleninpacheco@gmail.com>
2017-02-03 04:48:42 -0800

    Adding Sound

--- Project XIII/Assets/DestructibleRock.cs
+++ Project XIII/Assets/DestructibleRock.cs
@@ -4,5 +4,17 @@ using UnityEngine;
 
 public class DestructibleRock : ItemHitTrigger
 {
-	
+    public GameObject rockFragments;
+    protected override void ClassSpecificStart()
+    {
+        rockFragments = Instantiate(rockFragments);
+        rockFragments.transform.position = transform.position;
+        rockFragments.transform.parent = transform;
+    }
+    public override void ItemHit()
+    {
+        PlayShake();
+        GetComponent<AudioSource>().Play();
+        rockFragments.GetComponent<ParticleSystem>().Play();
+    }
 }
