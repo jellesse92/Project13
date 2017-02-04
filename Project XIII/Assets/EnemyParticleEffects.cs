14cbe43ae225ae1333e8959357736021f9539493
clpachec <carlleninpacheco@gmail.com>
2017-02-04 05:20:06 -0800

    Starting Enemy Particle Script

---
+++ Project XIII/Assets/EnemyParticleEffects.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyParticleEffects : ParticleEffects
+{
+    public GameObject deathParticles;
+    public GameObject coins;
+
+    protected override void ChildSpecificAwake()
+    {
+        InstantiateParticle(ref deathParticles);
+        InstantiateParticle(ref coins);
+    }
+}
