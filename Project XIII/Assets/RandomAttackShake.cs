8ca6d500952ccc5604abd6f3218c8900429c5cf0
clpachec <carlleninpacheco@gmail.com>
2017-02-07 05:08:40 -0800

    Added a random explosion on background shake. Need a good sound effect

---
+++ Project XIII/Assets/RandomAttackShake.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomAttackShake : MonoBehaviour {
+    CamShakeScript shake;
+
+    public float shakeMag;
+    public float shakeDur;
+    public float weightModifierBetweenShake;
+
+    GameObject rockFragment;
+    float secondToWait;
+	// Use this for initialization
+	void Start () {
+        shake = Camera.main.GetComponent<CamShakeScript>();
+        StartCoroutine(randomShake());
+        rockFragment = transform.GetChild(0).gameObject;
+    }
+	
+	// Update is called once per frame
+	void LateUpdate () {
+		
+	}
+
+    IEnumerator randomShake()
+    {
+        while (true)
+        {
+            secondToWait = Mathf.Clamp(Random.value * weightModifierBetweenShake, 5, weightModifierBetweenShake);
+            yield return new WaitForSeconds(secondToWait);
+            shake.StartShake(shakeMag, shakeDur);
+            GetComponent<AudioSource>().Play();
+            rockFragment.GetComponent<ParticleSystem>().Play();
+        }
+    }
+}
