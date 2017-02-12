d6c4844e4604bef7ca78f16c64ad57726d96343b
clpachec <carlleninpacheco@gmail.com>
2017-02-12 11:34:33 -0800

    Air Attack pivot adjustments

---
+++ Project XIII/Assets/LoadSceneManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadSceneManager : MonoBehaviour {
+    int sceneIndex = 2;
+
+    public void ActivateFadeScreenToLoad()
+    {//Use this to fade and animator will load scene
+        GetComponent<Animator>().SetTrigger("load");
+    }
+
+    public void ChangeSceneIndex(int index)
+    {//Use to change scene index
+        sceneIndex = index;
+    }
+
+	public void LoadScene()
+    {//Animator uses this to load the scene
+        SceneManager.LoadScene(sceneIndex);
+    }
+}
