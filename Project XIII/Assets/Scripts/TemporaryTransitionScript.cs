a146e23160bea74301ad4c23037164b41f3b3591
clpachec <carlleninpacheco@gmail.com>
2017-02-12 20:00:02 -0800

    Adding Load Scene Manager to the scenes

--- Project XIII/Assets/Scripts/TemporaryTransitionScript.cs
+++ Project XIII/Assets/Scripts/TemporaryTransitionScript.cs
@@ -3,14 +3,25 @@ using System.Collections;
 using UnityEngine.SceneManagement;
 
 public class TemporaryTransitionScript : MonoBehaviour {
-
     public int sceneIndex = 0;
-
+    LoadSceneManager loadSceneManager;
+    void Start()
+    {
+        GameObject loadSceneFound = GameObject.FindWithTag("LoadSceneManager");
+        if(loadSceneFound)
+            loadSceneManager = loadSceneFound.GetComponent<LoadSceneManager>();
+    }
 	void OnTriggerEnter2D(Collider2D col)
     {
         if(col.tag == "Player")
         {
-            SceneManager.LoadScene(sceneIndex);
+            if (loadSceneManager)
+            {
+                loadSceneManager.ChangeSceneIndex(sceneIndex);
+                loadSceneManager.ActivateFadeScreenToLoad();
+            }
+            else
+                SceneManager.LoadScene(sceneIndex);
         }
     }
 }
