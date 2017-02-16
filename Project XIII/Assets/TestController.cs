c7ff57f30329ace47bbb0853c4b3b4e1f581a544
Arielle Chongco <achongco@uci.edu>
2017-02-15 19:09:21 -0800

    Made testing scenes with controller easier

---
+++ Project XIII/Assets/TestController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestController : MonoBehaviour {
+
+    //SCRIPT IS FOR TESTING
+
+    public bool testingMode = false;                            //Determines if in testing mode
+
+    bool controllerJoined = false;                              //Determines if a controller has taken control of input
+    Transform playerList;
+
+	// Use this for initialization
+	void Start () {
+        playerList = GameObject.FindGameObjectWithTag("PlayerList").transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(testingMode)
+            if (!controllerJoined)
+                WatchForControllerJoin();
+	}
+
+    //Forces controller to take over
+    void WatchForControllerJoin()
+    {
+        for (int i = 1; i < 12; i++)
+            if (Input.GetButtonDown(i.ToString() + "_X"))
+            {
+                Debug.Log("TEST CONTROLLER JOIN");
+                controllerJoined = true;
+                for(int character = 0; character < 4; character++)
+                {
+                    playerList.GetChild(character).GetComponent<PlayerInput>().SetJoystickNum(i);
+                }
+            }
+    }
+
+
+}
