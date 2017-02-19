eb3e85662245b85e716e05274f3040433e1cd6a0
clpachec <carlleninpacheco@gmail.com>
2017-02-19 03:53:30 -0800

    Prepping Save Point

---
+++ Project XIII/Assets/Scripts/Data/DataManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+public static class DataManager{
+    public static GameData savedGame;
+
+    public static void SaveData()
+    {
+        savedGame = GameData.current;
+        BinaryFormatter binaryFormatter = new BinaryFormatter();
+        FileStream file = File.Create(Application.persistentDataPath + "/data.sav");
+        binaryFormatter.Serialize(file, savedGame);
+        file.Close();
+    }
+
+    public static void LoadData()
+    {
+        if (File.Exists(Application.persistentDataPath + "/data.sav"))
+        {
+            BinaryFormatter binaryFormatter = new BinaryFormatter();
+            FileStream file = File.Open(Application.persistentDataPath + "/savedGames.gd", FileMode.Open);
+            savedGame = (GameData)binaryFormatter.Deserialize(file);
+            file.Close();
+            GameData.current = savedGame;
+        }
+    }
+}
---
+++ Project XIII/Assets/Scripts/Data/SavePoint.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SavePoint : MonoBehaviour {
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.tag == "player")
+        {
+            GameData.current.scene = SceneManager.GetActiveScene().buildIndex;
+            GameData.current.playerPosition = collider.transform.position;
+            DataManager.SaveData();
+        }
+    }
+}
