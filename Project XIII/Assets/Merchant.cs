a6bf196f59680b9b29e5b9c327715e5bc789f343
clpachec <carlleninpacheco@gmail.com>
2017-02-20 06:05:14 -0800

    Created merchant

---
+++ Project XIII/Assets/Merchant.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Merchant : MonoBehaviour {
+    public GameObject levelUpUI;
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.tag == "Player")
+        {
+            levelUpUI.SetActive(true);
+        }
+    }
+}
