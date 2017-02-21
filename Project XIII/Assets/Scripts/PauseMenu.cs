d034922febaf3c251acd4d2bfe3e94d8c511cde2
jellesse92 <jehunter@uci.edu>
2017-02-21 02:27:38 -0800

    Adding Menu options for Pause Menu. Not finished, but sleepy >_>

--- Project XIII/Assets/Scripts/PauseMenu.cs
+++ Project XIII/Assets/Scripts/PauseMenu.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class PauseMenu : MonoBehaviour
 {
-
+    static string[] MenuOptions = { "Exit", "Player Stats", "Commands", "Settings" };
     GameController gc;
     AudioSource[] sfxObjects;
     public bool sfxSound = true;
+
     void Awake()
     {
         gc = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameController>();
