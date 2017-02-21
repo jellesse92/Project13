07e0baac58bae35ef47342e65ae8eeba52e96078
jellesse92 <jehunter@uci.edu>
2017-02-21 12:16:50 -0800

    Working Pause Menu with Left and right Arrow Keys

--- Project XIII/Assets/Scripts/PauseMenu.cs
+++ Project XIII/Assets/Scripts/PauseMenu.cs
@@ -4,22 +4,33 @@ using UnityEngine;
 
 public class PauseMenu : MonoBehaviour
 {
-    static string[] MenuOptions = { "Exit", "Player Stats", "Commands", "Settings" };
+    static string[] MenuOptions = { "Exit", "Stats", "Commands", "Settings" };
     GameController gc;
     AudioSource[] sfxObjects;
+    List<float> maxSfxVolumes;
     public bool sfxSound = true;
+    Animator anim;
 
     void Awake()
     {
+        maxSfxVolumes = new List<float>();
+        anim = GetComponentInChildren<Animator>();
         gc = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameController>();
         sfxObjects = FindObjectsOfType<AudioSource>();
+        foreach (AudioSource sfxObject in sfxObjects)
+        {
+            maxSfxVolumes.Add(sfxObject.volume);
+        }
     }
 
     void Update()
     {
-        foreach (AudioSource sfxObject in sfxObjects)
+        if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetAxis("Horizontal") < 0)
         {
-            sfxObject.volume = 0;
+            anim.SetTrigger("Left");
+        } else if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetAxis("Horizontal") > 0)
+        {
+            anim.SetTrigger("Right");
         }
     }
     void IncreaseSound()
