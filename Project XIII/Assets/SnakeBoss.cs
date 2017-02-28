21a26a64252b01469e87d63a22bf584d266bfa9a
clpachec <carlleninpacheco@gmail.com>
2017-02-28 05:25:34 -0800

    Starting boss script

---
+++ Project XIII/Assets/SnakeBoss.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnakeBoss : MonoBehaviour {
+    [System.Serializable]
+    public class TimeRange
+    {
+        public float minWait = 3f;
+        public float maxWait = 5f;
+    }
+
+    public int damage = 50;
+    public float timeBeforeFirstAttack = 2f;
+    public TimeRange timeRange;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	void LateUpdate () {
+		
+	}
+
+    void OnTriggerEnter2D(Collider2D triggerObject)
+    {
+        if (triggerObject.CompareTag("Player"))
+            DamagePlayer(triggerObject.gameObject);
+    }
+
+
+    void DamagePlayer(GameObject player)
+    {
+        player.GetComponent<PlayerProperties>().TakeDamage(damage);
+    }
+}
